bff8871fa295fe201f837e7ab0e12c8cf9deb872
Ak all bots Control <akallbotscontrol@gmail.com>
2025-07-04 10:26:15 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,3 @@
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
-# Never Give DATABASE_URI And BOT_TOKEN Here If Repo Is Forked Or Public Because This May Lead Account Ban.
 
 import os
 from os import environ
@@ -13,4 +8,4 @@ BOT_TOKEN    = environ.get("BOT_TOKEN", "")
 DATABASE_URI = environ.get("DATABASE_URI", "")
 LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", ""))
 ADMIN        = int(environ.get("ADMIN", ""))
-CHANNEL      = environ.get("CHANNEL", "@VJ_Botz")
+CHANNEL      = environ.get("CHANNEL", "🥰@RMCBACKUP😘")
